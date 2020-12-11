66aede8005ee70edd9aaa716776e1e7eccef4b0b
whdkfhr <whwoals18@naver.com>
2020-12-11 14:21:24 +0900

    순수 jdbc 작성

--- src/main/java/me/arok/hellospring/SpringConfig.java
+++ src/main/java/me/arok/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package me.arok.hellospring;
 
+import me.arok.hellospring.repository.JdbcMemberRepository;
 import me.arok.hellospring.repository.MemberRepository;
 import me.arok.hellospring.repository.MemoryMemberRepository;
 import me.arok.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
